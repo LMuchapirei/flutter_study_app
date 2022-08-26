a2f3e735564d4411b981b8bc742b7e2b34ade8bc
LMuchapirei <h170407h@hit.ac.zw>
2022-08-26 16:09:10 +0200

    Zoom controller to handle opening and closing of drawer

--- lib/controllers/zoom_drawer_controller.dart
+++ lib/controllers/zoom_drawer_controller.dart
@@ -1,10 +1,35 @@
 import 'package:flutter_zoom_drawer/flutter_zoom_drawer.dart';
 import 'package:get/get.dart';
+import 'package:url_launcher/url_launcher.dart';
 
 class MyZoomDrawerController extends GetxController {
   final zoomDrawerController = ZoomDrawerController();
+
+  @override
+  void onReady() {
+    super.onReady();
+  }
+
   void toggleDrawer() {
     zoomDrawerController.toggle?.call();
     update();
+  }
+
+  void signOut() {}
+
+  void signIn() {}
+
+  void webSite() {}
+
+  void email() {
+    final Uri emailLaunchUri =
+        Uri(scheme: 'mailto', path: 'muchapirei@gmail.com');
+    _launch(emailLaunchUri.toString());
+  }
+
+  Future<void> _launch(String url) async {
+    if (!await launch(url)) {
+      throw 'could not launch';
+    }
   }
 }
