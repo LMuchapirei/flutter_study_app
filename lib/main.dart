f0366a4c44fe9efd0cd0eaa37caf5046fbdf6b89
LMuchapirei <h170407h@hit.ac.zw>
2022-09-02 11:53:31 +0200

    remove debug banner

--- lib/main.dart
+++ lib/main.dart
@@ -29,6 +29,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return GetMaterialApp(
+        debugShowCheckedModeBanner: false,
         theme: Get.find<ThemeController>().lightTheme,
         getPages: AppRoutes.routes());
   }
