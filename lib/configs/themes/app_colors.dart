4e4f48b1d81728e1347e23662266fa4557d06d99
LMuchapirei <h170407h@hit.ac.zw>
2022-11-27 11:57:03 +0200

    Colors for handling the answers in quiz section

--- lib/configs/themes/app_colors.dart
+++ lib/configs/themes/app_colors.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:flutter_study_app/configs/themes/app_dark_theme.dart';
 import 'package:flutter_study_app/configs/themes/app_light_theme.dart';
 import 'package:flutter_study_app/configs/themes/ui_parameters.dart';
+import 'package:get/get.dart';
 
 const Color onSurfaceTextColor = Colors.white;
 const mainGradientLight = LinearGradient(
@@ -25,3 +26,11 @@ const customBlackColor = Color(0xFF2e3c62);
 
 const colorLightGreen = Color(0xff986fb98);
 const colorLightRed = Colors.red;
+
+Color answerSelectedColor() => UIParameters.isDarkMode()
+    ? Theme.of(Get.context!).cardColor.withOpacity(0.5)
+    : Theme.of(Get.context!).primaryColor;
+
+Color answerBorderColor() => UIParameters.isDarkMode()
+    ? const Color.fromARGB(255, 20, 46, 158)
+    : const Color.fromARGB(255, 221, 221, 221);
