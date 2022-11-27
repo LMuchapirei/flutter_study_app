1f2a669e79c17f9ac0f06ad8597d1c1d4654e06c
LMuchapirei <h170407h@hit.ac.zw>
2022-11-27 12:01:16 +0200

    Answer card to show in the quiz section

---
+++ lib/widgets/questions/answer_card.dart
@@ -0,0 +1,39 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_study_app/configs/themes/app_colors.dart';
+import 'package:flutter_study_app/configs/themes/ui_parameters.dart';
+
+class AnswerCard extends StatelessWidget {
+  const AnswerCard(
+      {Key? key,
+      required this.answer,
+      this.isSelected = false,
+      required this.onTap})
+      : super(key: key);
+  final String answer;
+  final bool isSelected;
+  final VoidCallback onTap;
+  @override
+  Widget build(BuildContext context) {
+    return InkWell(
+      borderRadius: UIParameters.cardBorderRadius,
+      onTap: onTap,
+      child: Ink(
+        child: Text(
+          answer,
+          style: TextStyle(
+            color: isSelected ? onSurfaceTextColor : null,
+          ),
+        ),
+        padding: const EdgeInsets.symmetric(vertical: 20, horizontal: 10),
+        decoration: BoxDecoration(
+            borderRadius: UIParameters.cardBorderRadius,
+            color: isSelected
+                ? answerSelectedColor()
+                : Theme.of(context).cardColor,
+            border: Border.all(
+                color:
+                    isSelected ? answerSelectedColor() : answerBorderColor())),
+      ),
+    );
+  }
+}
