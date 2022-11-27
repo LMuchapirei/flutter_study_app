2e98e82f0e2d2e5fe7f11773ce4e2918b52631b1
LMuchapirei <h170407h@hit.ac.zw>
2022-11-27 12:00:32 +0200

    Add the question card for each quiz

--- lib/screens/question/question_screen.dart
+++ lib/screens/question/question_screen.dart
@@ -440,12 +440,15 @@
 **/
 
 import 'package:flutter/material.dart';
+import 'package:flutter_study_app/configs/themes/custom_text_styles.dart';
 import 'package:flutter_study_app/firebase_ref/loading_status.dart';
 import 'package:flutter_study_app/widgets/common/question_place_holder.dart';
+import 'package:flutter_study_app/widgets/questions/answer_card.dart';
 import 'package:get/get.dart';
 import 'package:flutter_study_app/controllers/question_paper/questions_controller.dart';
 
 import '../../widgets/common/background_decoration.dart';
+import '../../widgets/content_area.dart';
 
 class QuestionScreen extends GetView<QuestionsController> {
   // lets our view know about the controller
@@ -459,14 +462,53 @@ class QuestionScreen extends GetView<QuestionsController> {
         (() => Column(
               children: [
                 if (controller.loadingStatus.value == LoadingStatus.loading)
-                  const Expanded(child: Center(child: QuestionScreenHolder())),
+                  const Expanded(
+                      child: ContentArea(child: QuestionScreenHolder())),
                 if (controller.loadingStatus.value == LoadingStatus.completed)
                   Expanded(
-                    child: SingleChildScrollView(
-                      child: Column(
-                        children: [
-                          Text(controller.currentQuestion.value!.question),
-                        ],
+                    child: ContentArea(
+                      child: SingleChildScrollView(
+                        padding: EdgeInsets.only(top: 25),
+                        child: Column(
+                          children: [
+                            Text(
+                              controller.currentQuestion.value!.question,
+                              style: questionTS,
+                            ),
+                            GetBuilder<QuestionsController>(
+                              // Specify what type of controller is being reacted to, so we can hook into its state and underlying methods
+                              id: "answers_list",
+                              builder: (controller) {
+                                return ListView.separated(
+                                  shrinkWrap: true,
+                                  padding: const EdgeInsets.only(top: 25),
+                                  physics: NeverScrollableScrollPhysics(),
+                                  itemBuilder: ((context, index) {
+                                    final answer = controller
+                                        .currentQuestion.value!.answers[index];
+                                    return AnswerCard(
+                                      isSelected: answer.identifier ==
+                                          controller.currentQuestion.value!
+                                              .selectedAnswer,
+                                      answer:
+                                          "${answer.identifier}. ${answer.answer}",
+                                      onTap: () {
+                                        controller
+                                            .selectedAnswer(answer.identifier);
+                                      },
+                                    );
+                                  }),
+                                  separatorBuilder: (context, index) =>
+                                      const SizedBox(
+                                    height: 10,
+                                  ),
+                                  itemCount: controller
+                                      .currentQuestion.value!.answers.length,
+                                );
+                              },
+                            )
+                          ],
+                        ),
                       ),
                     ),
                   ),
