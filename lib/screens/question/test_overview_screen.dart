ef674fe0dd3a21e950415f234c61f76b040b8925
LMuchapirei <h170407h@hit.ac.zw>
2022-11-29 19:52:33 +0200

    Test overview setup with the question cards using gridview builder

--- lib/screens/question/test_overview_screen.dart
+++ lib/screens/question/test_overview_screen.dart
@@ -1,7 +1,13 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_study_app/configs/themes/custom_text_styles.dart';
+import 'package:flutter_study_app/configs/themes/ui_parameters.dart';
 import 'package:flutter_study_app/controllers/question_paper/questions_controller.dart';
 import 'package:flutter_study_app/widgets/common/background_decoration.dart';
+import 'package:flutter_study_app/widgets/content_area.dart';
 import 'package:flutter_study_app/widgets/custom_app_bar.dart';
+import 'package:flutter_study_app/widgets/questions/answer_card.dart';
+import 'package:flutter_study_app/widgets/questions/countdown_timer.dart';
+import 'package:flutter_study_app/widgets/questions/question_number_card.dart';
 import 'package:get/get.dart';
 
 class TestOverviewScreen extends GetView<QuestionsController> {
@@ -13,11 +19,60 @@ class TestOverviewScreen extends GetView<QuestionsController> {
     return Scaffold(
       extendBodyBehindAppBar: true,
       appBar: CustomAppBar(
-        title: "Test over",
+        title: controller.completedTest,
       ),
       body: BackgroundDecoration(
-        child: Center(child: Text("Test ")),
-      ),
+          child: Column(
+        children: [
+          Expanded(
+              child: ContentArea(
+                  child: Column(
+            children: [
+              Row(
+                children: [
+                  CountdownTimer(
+                      color: UIParameters.isDarkMode()
+                          ? Theme.of(context).textTheme.bodyText1!.color
+                          : Theme.of(context).primaryColor,
+                      time: ''),
+                  Obx(
+                    () => Text(
+                      '${controller.time} Remaining',
+                      style: countDownTimerTs(),
+                    ),
+                  )
+                ],
+              ),
+              const SizedBox(
+                height: 20,
+              ),
+              Expanded(
+                child: GridView.builder(
+                    shrinkWrap: true,
+                    physics: BouncingScrollPhysics(),
+                    itemCount: controller.allQuestions.length,
+                    gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+                      crossAxisCount: Get.width ~/ 75,
+                      childAspectRatio: 1,
+                      crossAxisSpacing: 8,
+                      mainAxisSpacing: 8,
+                    ),
+                    itemBuilder: (_, index) {
+                      AnswerStatus? _answerStatus;
+                      if (controller.allQuestions[index].selectedAnswer !=
+                          null) {
+                        _answerStatus = AnswerStatus.answered;
+                      }
+                      return QuestionNumberCard(
+                          index: index + 1,
+                          status: _answerStatus,
+                          onTap: () {});
+                    }),
+              )
+            ],
+          )))
+        ],
+      )),
     );
   }
 }
