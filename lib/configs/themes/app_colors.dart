8e5545d74153c9dbb83dfa73dc8156309fd0a36f
LMuchapirei <h170407h@hit.ac.zw>
2022-11-29 19:52:01 +0200

    Answer colors

--- lib/configs/themes/app_colors.dart
+++ lib/configs/themes/app_colors.dart
@@ -5,6 +5,9 @@ import 'package:flutter_study_app/configs/themes/ui_parameters.dart';
 import 'package:get/get.dart';
 
 const Color onSurfaceTextColor = Colors.white;
+const Color correctAnswerColor = Color(0xFF3ac3cb);
+const Color wrongAnswerColor = Color(0xFFf85187);
+const Color notAnsweredColor = Color(0xFF2a3c65);
 const mainGradientLight = LinearGradient(
     begin: Alignment.topLeft,
     end: Alignment.bottomRight,
