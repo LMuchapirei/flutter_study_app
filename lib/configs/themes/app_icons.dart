0a1040e732b11f94be22de66dcbe6561648f507a
LMuchapirei <h170407h@hit.ac.zw>
2022-11-29 18:25:43 +0200

    Add menu icon though not appearing as expected

--- lib/configs/themes/app_icons.dart
+++ lib/configs/themes/app_icons.dart
@@ -8,4 +8,5 @@ class AppIcons {
   static const IconData menuLeft = IconData(0xe805, fontFamily: fontFam);
   static const IconData peace = IconData(0xe806, fontFamily: fontFam);
   static const IconData gridMenu4_4 = IconData(0xf7c0, fontFamily: fontFam);
+  static const IconData menu = IconData(0xe804, fontFamily: fontFam);
 }
