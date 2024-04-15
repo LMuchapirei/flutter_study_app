159dc7c4f2293ef327999871d05cf143756d554d
LMuchapirei <muchapireil@gmail.com>
2024-04-15 10:09:15 +0200

    Add file upload time

--- lib/controllers/chat_documents/document_model.dart
+++ lib/controllers/chat_documents/document_model.dart
@@ -1,18 +1,21 @@
 class PDFDocument {
   String? documentName;
   String? downloadUrl;
+  String? uploadTime;
 
   PDFDocument({this.documentName, this.downloadUrl});
 
   PDFDocument.fromJson(Map<String, dynamic> json) {
     documentName = json['documentName'];
     downloadUrl = json['downloadUrl'];
+    uploadTime = json['uploadTime'];
   }
 
   Map<String, dynamic> toJson() {
     final Map<String, dynamic> data = new Map<String, dynamic>();
     data['documentName'] = this.documentName;
     data['downloadUrl'] = this.downloadUrl;
+    data['uploadTime'] = this.uploadTime;
     return data;
   }
 
@@ -21,7 +24,8 @@ class PDFDocument {
     // TODO: implement toString
     return {
           "documentName":this.documentName,
-          "downloadUrl":this.downloadUrl
+          "downloadUrl":this.downloadUrl,
+          "uploadTime": this.uploadTime,
       }.toString();
   }
 }
