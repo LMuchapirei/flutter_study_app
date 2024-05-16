a2d313111b8d41e41e583a853ffc06268266e614
LMuchapirei <muchapireil@gmail.com>
2024-05-16 15:35:12 +0200

    feat: Add callbacks to custom formfield

--- lib/widgets/common/custom_form_field.dart
+++ lib/widgets/common/custom_form_field.dart
@@ -8,23 +8,31 @@ class CustomFormField extends StatelessWidget {
   final List<TextInputFormatter>? inputFormatters;
   final String? Function(String?)? validator;
   final void Function(String?)? onChange;
+  final TextInputType? textInputType;
+  final bool requiresPadding;
+  final void Function(String?)? onSaved;
   const CustomFormField({
     Key? key,
     this.inputFormatters,
     this.validator,
     this.onChange,
-    required this.hintText
+    this.onSaved,
+    this.textInputType,
+    required this.hintText,
+    this.requiresPadding = true
     }) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
     return Padding(
-      padding: UIParameters.mobileScreenPadding,
+      padding: requiresPadding ? UIParameters.mobileScreenPadding: EdgeInsets.all(0),
       child: TextFormField(
         inputFormatters: inputFormatters,
+        keyboardType: textInputType,
         validator: validator,
         decoration: UIParameters.inputDecoration(hintText: hintText),
         onChanged: onChange,
+        onSaved: onSaved,
       ),
 
     );
