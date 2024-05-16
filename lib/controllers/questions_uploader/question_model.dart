93bb7411c1d1609f47d0be130f80dd9c99de81ec
LMuchapirei <muchapireil@gmail.com>
2024-05-16 15:34:24 +0200

    feat: Model to capture the questions subject

---
+++ lib/controllers/questions_uploader/question_model.dart
@@ -0,0 +1,23 @@
+
+
+class QuestionSubject {
+   String? subject;
+   int? numberOfQuestions;
+
+   QuestionSubject({
+     this.numberOfQuestions,
+     this.subject});
+
+
+  QuestionSubject.fromJson(Map<String,dynamic> json){
+    numberOfQuestions = json["numberOfQuestions"];
+    subject = json["subject"];
+  }
+
+  Map<String,dynamic> toJson(){
+    final Map<String,dynamic> data = {};
+    data["subject"] = subject;
+    data["numberOfQuestions"] = numberOfQuestions;
+    return data;
+  }
+}
